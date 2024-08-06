31e02ec6bbc97476332a5750e8c73228006339d1
SladeBennett <slade8colt@gmail.com>
2024-08-05 19:51:09 -0600

    server link established

--- api/server.js
+++ api/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const server = express();
 
+server.use(express.json())
+
+server.get('/', (req, res) => {
+    res.send(`<h1>Hello from server</h1>`)
+})
+
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+
+server.use('*', (req, res) => {
+    res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` })
+})
 
 module.exports = server;
