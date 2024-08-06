f042e6232f874d0581238f8311296e287ea4f22d
SladeBennett <slade8colt@gmail.com>
2024-08-05 20:29:46 -0600

    Projects Router scaffolded

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,32 @@
-// Write your "projects" router here!
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+    console.log('hello from GET')
+})
+router.get('/:id', (req, res, next) => {
+    console.log('hello from GET ID')
+})
+router.post('/', (req, res, next) => {
+    console.log('hello from POST')
+})
+router.put('/:id', (req, res, next) => {
+    console.log('hello from PUT')
+})
+router.delete('/:id', (req, res, next) => {
+    console.log('hello from DELETE')
+})
+router.get('/:id/actions', (req, res, next) => {
+    console.log('hello from GET ACTIONS')
+})
+
+router.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        customMessage: 'disaster strikes in projects router',
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const server = express();
+const projectsRouter = require('./projects/projects-router')
 
 server.use(express.json())
+
+server.use('/api/projects', projectsRouter)
 
 server.get('/', (req, res) => {
     res.send(`<h1>Hello from server</h1>`)
 })
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
 
 server.use('*', (req, res) => {
     res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` })
